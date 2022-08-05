efc9ef88cb24827ad446c82f1581881fb889c39e
Isha Kunwar <74894810+vizualaid@users.noreply.github.com>
2022-08-06 00:00:30 +0530

    Create veryBigNum.kt

---
+++ veryBigNum.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // do not touch the lines above
+    // declare and initialize a variable here
+    val bigNumber = 10_230_167_123
+
+    // do not touch the lines below
+    println(bigNumber)
+}
