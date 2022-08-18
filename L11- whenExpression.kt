181ca7978d090dd89c924ca32e4368fa5d59caeb
Isha Kunwar <74894810+vizualaid@users.noreply.github.com>
2022-08-18 12:59:59 +0530

    Update L11- whenExpression.kt

--- L11- whenExpression.kt
+++ L11- whenExpression.kt
@@ -14,4 +14,38 @@ fun main(args:Array<String>){
 
     }
     println(res)
+ 
+    val a = 12
+    val b = 5
+
+
+
+//The when construct in Kotlin can be thought of as a replacement for Java switch Statement. 
+//It evaluates a section of code among many alternatives.
+val operator = readLine()
+
+    val result = when (operator) {
+        "+" -> a + b
+        "-" -> a - b
+        "*" -> a * b
+        "/" -> a / b
+        else -> "$operator operator is invalid operator."
+    }  println(result)
+    
+    //In the above example, we used when as an expression. However, it's not mandatory to use when as an expression. For example,
+     val aa = 12
+    val bb = 5
+
+    println("Enter operator either +, -, * or /")
+    val operator = readLine()
+
+    when (operator) {
+        "+" -> println("$aa + $bb = ${aa + bb}")
+        "-" -> println("$aa - $bb = ${aa - bb}")
+        "*" -> println("$aa * $bb = ${aa * bb}")
+        "/" -> println("$aa / $bb = ${aa / bb}")
+        else -> println("$operator is invalid")
+    }
+
+
 }
