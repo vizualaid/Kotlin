86b62cc1d0b9fbdafae9f7e5f55291fd1af1a10c
Isha Kunwar <74894810+vizualaid@users.noreply.github.com>
2022-08-18 12:44:23 +0530

    Create L11- whenExpression.kt

---
+++ L11- whenExpression.kt
@@ -0,0 +1,17 @@
+// Kotlin When Expression is similar to c,c++,and Java
+
+
+fun main(args:Array<String>){
+    val day=2
+    val res=when(day){
+        1->"Monday"
+        2->"Tuesday"
+        3->"wednesday"
+        4->"Thursday"
+        5->"Friday"
+        6->"saturday"
+        7->"Sunday"
+        else->"Invalid Input"
+
+    }
+}
