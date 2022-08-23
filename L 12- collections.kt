b557a5331aa7476e1ea63882d5f6ff05c142ae23
Isha Kunwar <74894810+vizualaid@users.noreply.github.com>
2022-08-23 12:59:08 +0530

    Update L 12- collections.kt

--- L 12- collections.kt
+++ L 12- collections.kt
@@ -1,9 +1,20 @@
+/*List<T> stores elements in a specified order and provides indexed access to them.
+Indices start from zero – the index of the first element – and go to lastIndex which is the (list.size - 1).
+List elements (including nulls) can duplicate: a list can contain any number of equal objects or occurrences of a single object.
+Two lists are considered equal if they have the same sizes and structurally equal elements at the same positions.
+
+
+LIST- unordered, duplicates allowed, 
+SET- Ordered, unique elements,
+*/
 fun printAll(strings: Collection<String>) {
     for(s in strings) print("$s ")
     println()
 }
 
 fun main() {
+   
+    //Collection
     val stringList = listOf("one", "two", "one")
     println(stringList)
  //  stringList = listOf("one","two", "two", "one")
@@ -31,4 +42,18 @@ fun main() {
            print("No")
     println("Index of 2:"+ theList.indexOf(2))
 
+//MutableCollection<T> is a Collection with write operations, such as add and remove.
+    
+    
+    //MutableList<T> is a List with list-specific write operations, 
+    //for example, to add or remove an element at a specific position.
+     val numbers = mutableListOf(1, 2, 3, 4)
+    numbers.add(5)
+    numbers.removeAt(1)
+    numbers[0] = 0
+    numbers.shuffle()
+    println(numbers)
+    
+    
+    
 }
